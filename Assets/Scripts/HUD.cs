727af72d94577070cf76c701d5ce417096e94c61
Simon Magnusson <simon.magnusson@student.forsbergsskola.se>
2020-10-20 14:26:40 +0200

    Added text that shows amount of gold

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Text goldAmountText;
+
+    private int getAmountOfGold;
+
+    public int GetAmountOfGold
+    {
+        get => getAmountOfGold;
+        set => getAmountOfGold = GetComponentInParent<Gold>().goldAmount;
+    }
+
+
+    void Update()
+    {
+        this.goldAmountText.text = $"{this.getAmountOfGold} Gold";
+    }
+}
