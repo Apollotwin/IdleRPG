666b2cbbf6652676868edc0f899dddd6022e7953
Simon Magnusson <simon.magnusson@student.forsbergsskola.se>
2020-10-20 10:33:53 +0200

    Added HUD
    Canvas Text added:
    Displays "Gold:" + int gold = 0;

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public int gold = 0;
+
+    public Text goldDisplay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.goldDisplay.text = $"Gold: {gold}";
+    }
+}
